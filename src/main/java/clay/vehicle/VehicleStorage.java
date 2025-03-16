bad68ee7429ad8864042bace8e9e08fbf03e3467
Pixel-Clay <code.clayt@gmail.com>
2025-03-16 21:36:32 +0300

    Create stub for VehicleStorage

---
+++ src/main/java/clay/vehicle/VehicleStorage.java
@@ -0,0 +1,24 @@
+package clay.vehicle;
+
+import clay.vehicle.vehicles.Vehicle;
+
+import java.util.HashMap;
+
+public class VehicleStorage {
+    HashMap<Integer, Vehicle> storage = new HashMap<>();
+
+    public void insert(Vehicle vehicle) {
+    }
+
+    public Vehicle[] dumpElements() {
+        return null;
+    }
+
+    public Vehicle getElement(int id) {
+        return null;
+    }
+
+    public void setElement(int id, Vehicle vehicle) {
+    }
+}
+
