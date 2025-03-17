99d02d6c6f27c0d5f998da9f1ff162316d138bad
Pixel-Clay <code.clayt@gmail.com>
2025-03-17 09:04:33 +0300

    Add getters, setters to Coordinates

--- src/main/java/clay/vehicle/vehicles/Coordinates.java
+++ src/main/java/clay/vehicle/vehicles/Coordinates.java
@@ -1,5 +1,10 @@
 package clay.vehicle.vehicles;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Coordinates {
   private double x; // Значение поля должно быть больше -731
   private double y; // Максимальное значение поля: 803
