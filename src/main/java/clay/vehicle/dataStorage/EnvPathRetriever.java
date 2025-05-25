062e1d25b6156c8c51f05274d462a1f029b006cb
Pixel-Clay <code.clayt@gmail.com>
2025-05-25 21:10:39 +0300

    Document EnvPathRetriever

--- src/main/java/clay/vehicle/dataStorage/EnvPathRetriever.java
+++ src/main/java/clay/vehicle/dataStorage/EnvPathRetriever.java
@@ -1,17 +1,26 @@
 package clay.vehicle.dataStorage;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Utility class for retrieving file paths from environment variables. This class provides
+ * functionality to convert environment variable values into valid file system paths.
+ */
 public class EnvPathRetriever {
-  String varName;
-
-  public EnvPathRetriever(String varName) {
-    this.varName = varName;
-  }
-
-  public String getPath() throws NoEnvVarFoundException {
-    String path = System.getenv("CLAY_VEHICLE_DATA_PATH");
+  /**
+   * Retrieves a file path from the specified environment variable. The environment variable should
+   * contain a valid file system path.
+   *
+   * @param varName the name of the environment variable to retrieve
+   * @return a Path object representing the file path from the environment variable
+   * @throws NoEnvVarFoundException if the specified environment variable does not exist
+   */
+  public static Path getPath(String varName) throws NoEnvVarFoundException {
+    String path = System.getenv(varName);
     if (path == null) {
-      throw new NoEnvVarFoundException("Env var " + this.varName + " does not exist");
+      throw new NoEnvVarFoundException("Env var " + varName + " does not exist");
     }
-    return path;
+    return Paths.get(path);
   }
 }
